75d47cf633c0fec8863d7a4471a410c5c9fc06db
J-Santoyo <joanasantoyo030@gmail.com>
2022-03-07 13:59:54 -0700

    created routes file and required it in server js file

--- server.js
+++ server.js
@@ -12,3 +12,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+// require routes file
+require('./routes/routes')(app);
