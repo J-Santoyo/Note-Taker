f12ab1c959fe14f370a95cb0819dbfd80a66f245
J-Santoyo <joanasantoyo030@gmail.com>
2022-03-07 13:55:43 -0700

    setup data parsing

--- server.js
+++ server.js
@@ -6,3 +6,9 @@ const path = require('path');
 // Initialize express app
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Setup data parsing
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
