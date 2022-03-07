dbefa83a7e21fb433793098d3c6b9815052932cd
J-Santoyo <joanasantoyo030@gmail.com>
2022-03-07 14:08:02 -0700

    setup notes variable in routes js

--- routes/routes.js
+++ routes/routes.js
@@ -2,5 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = route1 => {
-    
+
+    //  notes variable
+    fs.readFile("db/db.json","utf8", (err, data) => {
+
+        if (err) throw err;
+
+        var notes = JSON.parse(data);
+    })
 }
